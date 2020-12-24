97850492c0f637432e9d145a0a72660f7cb80a2b
Michael Stead <mstead@redhat.com>
2020-12-24 09:04:47 -0400

    Created metering job for openshift metrics
    This job is scheduled to run every hour on the hour by default.

--- src/main/java/org/candlepin/subscriptions/ApplicationConfiguration.java
+++ src/main/java/org/candlepin/subscriptions/ApplicationConfiguration.java
@@ -23,7 +23,8 @@ package org.candlepin.subscriptions;
 import org.candlepin.subscriptions.capacity.CapacityIngressConfiguration;
 import org.candlepin.subscriptions.conduit.ConduitConfiguration;
 import org.candlepin.subscriptions.conduit.job.OrgSyncConfiguration;
-import org.candlepin.subscriptions.metering.MeteringWorkerConfiguration;
+import org.candlepin.subscriptions.metering.job.MeteringJobConfiguration;
+import org.candlepin.subscriptions.metering.worker.MeteringWorkerConfiguration;
 import org.candlepin.subscriptions.resource.ApiConfiguration;
 import org.candlepin.subscriptions.retention.PurgeSnapshotsConfiguration;
 import org.candlepin.subscriptions.security.SecurityConfig;
@@ -63,7 +64,7 @@ import javax.validation.Validator;
     CaptureSnapshotsConfiguration.class, PurgeSnapshotsConfiguration.class,
     LiquibaseUpdateOnlyConfiguration.class, TallyWorkerConfiguration.class, OrgSyncConfiguration.class,
     DevModeConfiguration.class, SecurityConfig.class, HawtioConfiguration.class,
-    MeteringWorkerConfiguration.class
+    MeteringWorkerConfiguration.class, MeteringJobConfiguration.class
 })
 public class ApplicationConfiguration implements WebMvcConfigurer {
     @Bean
