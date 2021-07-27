976c6758a675ef366ae9c533eca42af667a37e23
Michael Stead <mstead@redhat.com>
2021-07-27 11:46:09 -0300

    Made improvements to the EventJmxBean
    * Add delete event functionality
    * Add save multiple events functionality.
    * Fix up the event reponse data so that the data is a little
      more useful
    
    Deleting events is only supported in dev_mode.

--- src/main/java/org/candlepin/subscriptions/jmx/EventJmxBean.java
+++ src/main/java/org/candlepin/subscriptions/jmx/EventJmxBean.java
@@ -22,9 +22,10 @@ package org.candlepin.subscriptions.jmx;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.time.OffsetDateTime;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 import javax.transaction.Transactional;
 import org.candlepin.subscriptions.ApplicationProperties;
 import org.candlepin.subscriptions.event.EventController;
@@ -65,30 +66,66 @@ public class EventJmxBean {
   @ManagedOperationParameter(name = "accountNumber", description = "Account number")
   @ManagedOperationParameter(name = "begin", description = "Beginning of time range (inclusive)")
   @ManagedOperationParameter(name = "end", description = "End of time range (exclusive)")
-  public String fetchEventsInTimeRange(String accountNumber, String begin, String end) {
+  public List<Event> fetchEventsInTimeRange(String accountNumber, String begin, String end) {
     OffsetDateTime beginValue = OffsetDateTime.parse(begin);
     OffsetDateTime endValue = OffsetDateTime.parse(end);
-    Stream<Event> eventStream =
-        eventController.fetchEventsInTimeRange(accountNumber, beginValue, endValue);
-    return String.format("[%s]", eventStream.map(Event::toString).collect(Collectors.joining(",")));
+    return eventController
+        .fetchEventsInTimeRange(accountNumber, beginValue, endValue)
+        .collect(Collectors.toList());
   }
 
   @ManagedOperation(description = "Fetch an event")
   @ManagedOperationParameter(name = "eventId", description = "Event UUID")
-  public String getEvent(String eventId) {
-    return eventController.getEvent(UUID.fromString(eventId)).toString();
+  public Event getEvent(String eventId) {
+    Optional<Event> event = eventController.getEvent(UUID.fromString(eventId));
+    return event.isPresent() ? event.get() : null;
+  }
+
+  @ManagedOperation(description = "Delete an event")
+  @ManagedOperationParameter(name = "eventId", description = "Event UUID")
+  public String deleteEvent(String eventId) {
+    if (!applicationProperties.isDevMode()) {
+      throw new UnsupportedOperationException("Can only delete events in dev mode!");
+    }
+
+    try {
+      eventController.deleteEvent(UUID.fromString(eventId));
+      return String.format("Successfully deleted Event with ID: %s", eventId);
+    } catch (Exception e) {
+      return String.format(
+          "Failed to delete Event with ID: %s  Cause: %s", eventId, e.getMessage());
+    }
   }
 
   @ManagedOperation(description = "Save an event. Supported only in dev-mode.")
   @ManagedOperationParameter(name = "json", description = "Event JSON")
-  public String saveEvent(String json) {
+  public Event saveEvent(String json) {
     if (!applicationProperties.isDevMode()) {
       throw new JmxException("Unsupported outside dev-mode!");
     }
     try {
-      return eventController.saveEvent(objectMapper.readValue(json, Event.class)).toString();
+      return eventController.saveEvent(objectMapper.readValue(json, Event.class));
     } catch (Exception e) {
       log.error("Error saving event", e);
+      throw new JmxException("Error saving event. See log for details.");
+    }
+  }
+
+  @ManagedOperation(description = "Save a list of events. Supported only in dev-mode.")
+  @ManagedOperationParameter(
+      name = "jsonListOfEvents",
+      description = "Event list specified as JSON")
+  public List<Event> saveEvents(String jsonListOfEvents) {
+    if (!applicationProperties.isDevMode()) {
+      throw new JmxException("Unsupported outside dev-mode!");
+    }
+    try {
+      return eventController.saveAll(
+          objectMapper.readValue(
+              jsonListOfEvents,
+              objectMapper.getTypeFactory().constructCollectionType(List.class, Event.class)));
+    } catch (Exception e) {
+      log.error("Error saving events", e);
       throw new JmxException("Error saving event. See log for details.");
     }
   }
